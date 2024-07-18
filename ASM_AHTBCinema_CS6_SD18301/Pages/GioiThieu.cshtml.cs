028e43f063c77f8104186af028ca4ce9720205e1
nnguyen5968 <pinnguyen801@gmail.com>
2024-07-18 23:13:27 +0700

    [Giaodien] - Gioithieu, Phimchuachieu, Thanhtoan (Done)

---
+++ ASM_AHTBCinema_CS6_SD18301/Pages/GioiThieu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASM_AHTBCinema_CS6_SD18301.Pages
+{
+    public class GioiThieuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
